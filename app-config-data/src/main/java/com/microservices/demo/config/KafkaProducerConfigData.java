946b86877e2dc7ff9a12d03e7f77e0b4178ba91b
Xavier Zabala M <xzabalam@gmail.com>
2022-08-25 15:09:57 -0500

    adding sprign config server

--- app-config-data/src/main/java/com/microservices/demo/config/KafkaProducerConfigData.java
+++ app-config-data/src/main/java/com/microservices/demo/config/KafkaProducerConfigData.java
@@ -11,10 +11,11 @@ public class KafkaProducerConfigData {
     private String keySerializerClass;
     private String valueSerializerClass;
     private String compressionType;
+    private String acks;
     private Integer batchSize;
     private Integer batchSizeBoostFactor;
     private Integer lingerMs;
     private Integer requestTimeoutMs;
     private Integer retryCount;
-    private String acks;
+
 }
